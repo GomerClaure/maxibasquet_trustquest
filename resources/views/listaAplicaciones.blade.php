fcf58884ee2f09fde592f41696977c35c3e858e1
jhosmanB <102555068+jhosmanB@users.noreply.github.com>
2022-09-29 20:27:39 -0400

    Tabla de aplicaciones de preinscripcion con boostrap 5

--- resources/views/listaAplicaciones.blade.php
+++ resources/views/listaAplicaciones.blade.php
@@ -19,26 +19,62 @@
                 font-family: 'Nunito', sans-serif;
             }
         </style>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="{{asset('css/StyleTablaAplicaciones.css')}}">
     </head>
     <body class="antialiased">
         <div class=" bg-gray-100 dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
-           <h2>Equipos Preinscritos</h2>
-           <h3>3er Torneo Internacional de Maxi Basquet</h3>
-           <table>
-                <tr> 
-                    <th>Nombre de Equipo </th>
-                    <th>Monto a Pagar</th>
-                    <th>Estado</th>
-                    <th>Detalles</th>
-                </tr>
-                @foreach ($aplicaciones as $aplicacion)
-                    <tr>
-                        <td>{{$aplicacion->NombreEquipo}}</td>
-                        <td>{{$aplicacion->Total}}</td>
-                        <td>{{$aplicacion->EstadoAplicacion}}</td>
-                    </tr>
-                @endforeach
-           </table>
+
+           <section class="intro">
+                <div class="bg-image h-100" style="background-color: #f5f7fa;">
+                    <div class="mask d-flex align-items-center h-100">
+                    <div class="container">
+                        <div class="row justify-content-center">
+                        <div class="col-12">
+                                <h3>Equipos Preinscritos</h3>
+                                <h5>3er Torneo Internacional de Maxi Basquet</h5>
+                            <div class="card">
+                                <div class="ps-3 py-2">
+                                    <h4>Lista Detallada</h4>
+                                </div>
+                            <div class="card-body  pt-0 ps-3 ">
+                                <div class="table-responsive table-scroll" data-mdb-perfect-scrollbar="true" style="position: relative; ">
+                                <table class="table table-striped table-borderless border-dark rounded-0 mb-0 text-center align-middle">
+                                    <thead style="background-color: #002d72;">
+                                    <tr>
+                                        <th>Nombre de Equipo </th>
+                                        <th>Monto a Pagar</th>
+                                        <th>Estado de Preinscripcion</th>
+                                        <th>Detalles</th>
+                                    </tr>
+                                    </thead>
+                                    <tbody>
+                                        @foreach ($aplicaciones as $aplicacion)
+                                        <tr>
+                                            <td>{{$aplicacion->NombreEquipo}}</td>
+                                            <td >{{$aplicacion->Total}} $</td>
+                                            <td>{{$aplicacion->EstadoAplicacion}}</td>
+                                            <td class="d-grid gap-2"><button type="button" class="btn btn-primary btn-sm rounded-0">
+                                                Detalles
+                                                <!-- enlace para mostrar los detalles del formulario de preinscripcion
+                                                <a href="{{url('ruta de verificar datos del formulario'/.$aplicacion->IdAplicacion )}}">Detalles</a>
+                                                -->
+                                            </button></td>
+                                        </tr>
+                                        @endforeach
+                                    </tbody>
+                                </table>
+                                </div>
+                            </div>
+                            </div>
+                        </div>
+                        </div>
+                    </div>
+                    </div>
+                </div>
+            </section>
         </div>
+
     </body>
 </html>
