d55ebb27afda79b9624e111cb5e75bffa4802402
jhosmanB <102555068+jhosmanB@users.noreply.github.com>
2022-10-02 13:36:24 -0400

    modelos para Equipo,Jugador,Persona

---
+++ app/Models/Equipo.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Equipo extends Model
+{
+    use HasFactory;
+    protected $table = "equipos";
+    protected $primaryKey = 'IdEquipo';
+    protected $fillable = ["IdAplicacion","NombreEquipo","LogoEquipo"];
+    protected $hidden = ["IdEquipo"];
+
+}
---
+++ app/Models/Jugador.php
@@ -0,0 +1,15 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Jugador extends Model
+{
+    use HasFactory;
+    protected $table = "jugadores";
+    protected $primaryKey = 'IdJugador';
+    protected $fillable = ["IdEquipo","IdCategoria","IdPais","IdPersona","PesoJugador","AlturaJugador","FotosCarnet","PosicionJugador","NumeroCamiseta","HabilitacionJugador"];
+    protected $hidden = ["IdJugador"];
+}
