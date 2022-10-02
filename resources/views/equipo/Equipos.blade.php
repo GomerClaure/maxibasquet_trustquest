f3ea77e1b07c028377c78ca3c8be4aeeed41d42b
Luisa Mora <luisamora895@gmail.com>
2022-10-02 12:31:36 -0400

    Pagina donde se mostrar equipo

---
+++ resources/views/equipo/Equipos.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Equipos Maxi Basquet</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+        <link rel="stylesheet" href="{{asset('css/StyleMostrarEquipo.css')}}">
+</head>
+<body>
+    <h1>Nombre del equipo</h1>
+</body>
+</html>
