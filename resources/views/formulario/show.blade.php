38e4ca8b7188891da70ba74df3176420f0838900
ronald <roaldtp@gmail.com>
2022-10-04 21:11:53 -0300

    interfaceV4

--- resources/views/formulario/show.blade.php
+++ resources/views/formulario/show.blade.php
@@ -7,7 +7,7 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-iYQeCzEYFbKjA/T2uDLTpkwGzCiq6soy8tYaI1GyVh/UjpbCx/TYkiZhlZB6+fzT" crossorigin="anonymous">
-    <link href="../css/preinscripcion.css" rel="stylesheet">
+    <link href="../css/formulario.css" rel="stylesheet">
     <title>Preinscripcion</title>
 </head>
 
@@ -29,7 +29,7 @@
                         </div>
                         <div class="col-md-6">
                             <label for="inputPassword4" class="form-label">Nombre del encargado:</label>
-                            <input class="form-control" id="inputPassword4" >
+                            <input class="form-control" id="inputPassword4" value="{{$datos->NombrePersona}}">
                         </div>
                         <div class="col-md-6">
                             <div>
@@ -37,60 +37,24 @@
                             </div>
 
                             <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-                                <label class="form-check-label" for="inlineCheckbox1">+30</label>
+                               
+                                <label class="form-check-label" for="inlineCheckbox1">{{$datos->NombreCategoria}}</label>
                             </div>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
-                                <label class="form-check-label" for="inlineCheckbox2">+35</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-                                <label class="form-check-label" for="inlineCheckbox1">+40</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
-                                <label class="form-check-label" for="inlineCheckbox2">+45</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-                                <label class="form-check-label" for="inlineCheckbox1">+50</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" id="inlineCheckbox2" value="option2">
-                                <label class="form-check-label" for="inlineCheckbox2">+55</label>
-                            </div>
-                            <div class="form-check form-check-inline">
-                                <input class="form-check-input" type="checkbox" id="inlineCheckbox1" value="option1">
-                                <label class="form-check-label" for="inlineCheckbox1">+60</label>
-                            </div>
+                            
                         </div>
                         <div class="col-md-6">
                             <label for="inputPassword4" class="form-label">Correo Electronico:</label>
-                            <input type="password" class="form-control" id="inputPassword4">
+                            <input class="form-control" id="inputPassword4" value="{{$datos->email}}">
                         </div>
                         <div class="col-md-6">
-                            <label for="inputEmail4" class="form-label">Pais:</label>
-                            <select class="form-select" id="exampleFormControlSelect1">
-                                <option>Argentina</option>
-                                <option>Bolivia</option>
-                                <option>Brasil</option>
-                                <option>Chile</option>
-                                <option>Colombia</option>
-                                <option>Ecuador</option>
-                                <option>Guyana</option>
-                                <option>Paraguay</option>
-                                <option>Peru</option>
-                                <option>Uruguay</option>
-                                <option>Venezuela</option>
-                            </select>
+                            <label for="pais" class="form-label">Pais:</label>
+                            <input type="text" id="pais" value="{{$datos->NombrePais}}">
                         </div>
                         <div class="col-md-6">
                             <label for="inputPassword4" class="form-label">Telefono de contacto:</label>
                             <div class="input-group">
-                                <span class="input-group-text" id="inputGroup-sizing-sm">+591</span>
                                 <!-- <input disabled class="input-group-text p-0 " value="+591"> -->
-                                <input type="text" class="form-control" id="specificSizeInputGroupUsername">
+                                <input type="text" class="form-control" id="specificSizeInputGroupUsername" value="{{$datos->NumeroDelegado}}">
                             </div>
                         </div>
                     </div>
@@ -100,28 +64,30 @@
                         <hr>
                         <div class="col-md-6">
                             <label for="inputEmail4" class="form-label">Nro de Transcción:</label>
-                            <input type="email" class="form-control" id="inputEmail4">
+                            <input type="email" class="form-control" id="inputEmail4" value="{{$datos->NumeroTransaccion}}">
                         </div>
                         <div class="col-md-6">
                             <label for="inputPassword4" class="form-label">Monto a pagar:</label>
-                            <input type="password" class="form-control" id="inputPassword4">
+                            <input class="form-control" id="inputPassword4" value="{{$datos->MontoTransaccion}}">
                         </div>
                         <div class="col-md-6">
                             <div>
                                 <label for="inputEmail4" class="form-label">Voucher de pago:</label>
                             </div>
                             <div class="input-group">
-                                <input type="file" class="form-control" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload">
+                                <input type="text" class="form-control" id="inputGroupFile04" aria-describedby="inputGroupFileAddon04" aria-label="Upload" value="{{$datos->FotoVaucher}}">
                             </div>
                         </div>
                         <div class="col-md-6">
                             <label for="inputPassword4" class="form-label">Fecha de depósito:</label>
-                            <input type="password" class="form-control" id="inputPassword4">
+                            <input class="form-control" id="inputPassword4" value="{{$datos->FechaTransaccion}}">
                         </div>
                     </div>
-                    <div class="col-md-12 text-center">
-                        <button type="submit" class="btn btn-primary">Preinscribir Equipo</button>
+                    <div class="col-md-12 text-right ">
+                        <button type="submit" class="btn btn-primary">aceptar</button>
+                        <button type="submit" class="btn btn-primary">rechazar</button>
                     </div>
+                    
                 </form>
             </section>
         </div>
