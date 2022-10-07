ed6ef758b3a0c39c818027b74c1cdc4cef513172
Luisa Mora <luisamora895@gmail.com>
2022-10-07 10:39:36 -0400

    cambios en estilo

--- resources/views/equipo/Equipos.blade.php
+++ resources/views/equipo/Equipos.blade.php
@@ -8,7 +8,10 @@
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" href="{{asset('css/StyleMostrarEquipo.css')}}">
-</head>
+        <link rel="preconnect" href="https://fonts.googleapis.com">
+<link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+<link href="https://fonts.googleapis.com/css2?family=Rubik:wght@900&display=swap" rel="stylesheet">
+      </head>
 <body>
 
 @foreach($c as $dato)
