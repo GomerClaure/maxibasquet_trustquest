04f1eb87276ac53637fdc1256f5e479bf4d277a8
ronald <roaldtp@gmail.com>
2022-10-16 14:49:39 -0300

    historia terminada agrado stylos

--- resources/views/listaequipos/listaEquipos.blade.php
+++ resources/views/listaequipos/listaEquipos.blade.php
@@ -21,7 +21,7 @@
         </style>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
-        <link rel="stylesheet" href="{{asset('css/StyleTablaAplicaciones.css')}}">
+        <link rel="stylesheet" href="{{asset('css/StyleListaEquipos.css')}}">
     </head>
     <body class="antialiased">
     <header >
