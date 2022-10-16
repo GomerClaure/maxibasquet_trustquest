35ff15671981a6907a263dc457959689ecad6003
ronald <roaldtp@gmail.com>
2022-10-16 14:47:40 -0300

    historia terminada

---
+++ app/Http/Controllers/ListaEquiposController.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Equipo;
+
+class ListaEquiposController extends Controller
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index()
+    {
+        $listas = Equipo::select(
+            'equipos.NombreEquipo',
+            'equipos.LogoEquipo'
+        )
+        ->get();
+        return view("listaequipos.listaEquipos",compact('listas'));
+    }
+
+    /**
+     * Show the form for creating a new resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function create()
+    {
+        //
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\Persona  $persona
+     * @return \Illuminate\Http\Response
+     */
+    public function show(Equipo $persona)
+    {
+        //
+    }
+
+    /**
+     * Show the form for editing the specified resource.
+     *
+     * @param  \App\Models\Persona  $persona
+     * @return \Illuminate\Http\Response
+     */
+    public function edit(Equipo $persona)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\Persona  $persona
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, Equipo $persona)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\Persona  $persona
+     * @return \Illuminate\Http\Response
+     */
+    public function destroy(Equipo $persona)
+    {
+        //
+    }
+}
