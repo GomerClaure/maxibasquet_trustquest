d3e8b6f30d09d502e490c16bc56ebf39b10cea51
Gomer Claure <gomerclaureb@gmail.com>
2022-10-17 13:23:22 -0400

    Cambiando imagen de previsualizacion

--- resources/views/logo/formularioSubidaLogo.blade.php
+++ resources/views/logo/formularioSubidaLogo.blade.php
@@ -22,11 +22,11 @@
     <div class="row">
         <div class="col-md-6">
             <label for="nombreDeEquipo" class="form-label">Nombre de equipo:</label>
-            <input name="nombreDeEquipo" type="text" class="form-control" id="nombreDeEquipo" value="{{ $equipo->NombreEquipo}}">
+            <input name="nombreDeEquipo" type="text" class="form-control" id="nombreDeEquipo" value="{{ $equipo->NombreEquipo}}" readonly>
         </div>
         <div class="col-md-6">
             <div class="input-group">
-                <img src="{{asset('storage').'/'.$equipo->LogoEquipo}}" width="362" height="203">
+                <img id='img_view'src="{{asset('storage').'/'.$equipo->LogoEquipo}}" width="362" height="203">
             </div>
             <div class="form-group">
                 <label for="">Logotipo del equipo:</label>
@@ -46,34 +46,6 @@
 <script>
     $(function(){
 
-        $('#form').on('submit', function(e){
-            e.preventDefault();
-
-            var form = this;
-            $.ajax({
-                url:$(form).attr('action'),
-                method:$(form).attr('method'),
-                data:new FormData(form),
-                processData:false,
-                dataType:'json',
-                contentType:false,
-                beforeSend:function(){
-                    $(form).find('span.error-text').text('');
-                },
-                success:function(data){
-                    if(data.code == 0){
-                        $.each(data.error, function(prefix,val){
-                            $(form).find('span.'+prefix+'_error').text(val[0]);
-                        });
-                    }else{
-                        $(form)[0].reset();
-                        // alert(data.msg);
-                        fetchAllProducts();
-                    }
-                }
-            });
-        });
-
         //Reset input file
         $('input[type="file"][name="product_image"]').val('');
         //Image preview
@@ -87,7 +59,8 @@
                       img_holder.empty();
                       var reader = new FileReader();
                       reader.onload = function(e){
-                          $('<img/>',{'src':e.target.result,'class':'img-fluid','style':'max-width:100px;margin-bottom:10px;'}).appendTo(img_holder);
+                          $('#img_view').attr('src', e.target.result);
+                        //   $('<img/>',{'src':e.target.result,'class':'img-fluid','style':'max-width:100px;margin-bottom:10px;'}).appendTo(img_holder);
                       }
                       img_holder.show();
                       reader.readAsDataURL($(this)[0].files[0]);
@@ -98,6 +71,7 @@
                 $(img_holder).empty();
             }
         });
+        
 
         
 
