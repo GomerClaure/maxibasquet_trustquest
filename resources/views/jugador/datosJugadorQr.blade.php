d7afe761749aea16de4c3c924dd76e84eae0b4cc
Gomer Claure <gomerclaureb@gmail.com>
2022-10-28 14:14:35 -0400

    Creando grilla de datos de forma ordenada

--- resources/views/jugador/datosJugadorQr.blade.php
+++ resources/views/jugador/datosJugadorQr.blade.php
@@ -10,87 +10,92 @@
 </head>
 </html>
 @section('content')
-<div class="container justify-content-center"">
+<div class="justify-content-center">
     <section class=" main-title text-center">
-        <h1 class="display-6 mb-0" >Datos del jugador</h1>
-        <p>3er Torneo Internacional de Maxi Basquet</p>
+        <h1>Datos del jugador</h1>
+        {{-- <p>3er Torneo Internacional de Maxi Basquet</p> --}}
     </section>
-    <section class="card">
-        <div class="row ">
-            <div class="col-md-3">
-                <div class="imagenJugador">
-                    <img src="{{asset('storage').'\uploads\persona.jpg'}}" alt="Foto del jugador">
-                </div>
-            </div>
-            <div class="col-md-6">
-                <div class="jugador"> <p> <b>{{"Los pumas del campo"}} {{"+60"}} | {{"Delantero"}}</b></p> </div>
-                <div class="nombreJugador">
-                    <p>{{"Este Banquito Lopez"}}</p>
-                </div>
-                {{-- <label for="fotoDelJugador" class="form-label">Nombre Completo:</label> --}}
-            </div>
-        </div>
-        <div class="row">
-            <div class="col-md-2">
+    <div class="relative  items-top justify-center min-h-screen dark:bg-gray-900 sm:items-center py-4 sm:pt-0">
                 <div class="row">
-                    <div class="col-lg-9">
-                        Level 1: .col-sm-3
-                      </div>
-                </div>
-                <div class="row filaDato">
-                    <div class="col-lg-9">
-                        Level 1: sm3
+                    <div class="col-1">
+                    </div>
+                    <div class="col">
+                        <div class="row mainCard">
+                            <div class="col">
+                                {{-- <div class="imagenJugador"> --}}
+                                    <img class="card-img-top imagenJugador" src="{{asset('storage').'\uploads\persona.jpg'}}" alt="Foto del jugador">
+                                {{-- </div> --}}
+                            </div>
+                            <div class="col-md-8">
+                                <div class="jugador"> <p> <b>{{"Los pumas del campo "}} | {{" +60" }} | {{" Delantero"}}</b></p> </div>
+                                <div class="nombreJugador">
+                                    <p>{{"Este Banquito Lopez"}}</p>
+                                </div>
+                            </div>
+                        </div>
+                        <div class="row data">
+                            <div class="col dosFilas">
+                                <div class="row filaDato">
+                                    <div class="col-lg-9">
+                                        <label for="" class="tituloDeCelda">Nro. Camiseta</label>
+                                        <br>
+                                        <label for="" class="contenidoCelda">Camiseta</label>
+                                      </div>
+                                </div>
+                                <div class="row filaDato">
+                                    <div class="col-lg-9">
+                                        <label for="" class="tituloDeCelda">Telefono</label>
+                                        <br>
+                                        <label for="" class="contenidoCelda">Camiseta</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col dosFilas" >
+                                <div class="row filaDato">
+                                    <div class="col-lg-9">
+                                        <label for="" class="tituloDeCelda">Nacionalidad</label>
+                                        <br>
+                                        <label for="" class="contenidoCelda">Camiseta</label>
+                                      </div>
+                                </div>
+                                <div class="row filaDato">
+                                    <div class="col-lg-9">
+                                        <label for="" class="tituloDeCelda">Fecha de nacimiento</label>
+                                        <label for="" class="contenidoCelda">Camiseta</label>
+                                    </div>
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="row celda">
+                                    <label for="" class="tituloDeUnaCelda">Nro. Documento de Identidad </label>
+                                    <label for="" class="contenidoCelda nro">8745698563</label>
+                                </div>
+                                
+                            </div>
+                            <div class="col-md-2">
+                                <div class="row celda">
+                                    <label for="" class="tituloDeUnaCelda">Edad</label>
+                                    <label for="" class="contenidoCelda">Camiseta</label>
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="row celda">
+                                    <label for="" class="tituloDeUnaCelda">Peso</label>
+                                    <label for="" class="contenidoCelda">Camiseta</label>
+                                </div>
+                            </div>
+                            <div class="col-md-2">
+                                <div class="row celda">
+                                    <label for="" class="tituloDeUnaCelda">Altura</label>
+                                    <label for="" class="contenidoCelda">Camiseta</label>
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-1">
                     </div>
                 </div>
-            </div>
-            <div class="col-md-2">
-                <div class="row filaDato">
-                    <div class="col-lg-9">
-                        Level 1: .col-sm-3
-                      </div>
-                </div>
-                <div class="row">
-                    <div class="col-lg-9">
-                        Level 1: sm3
-                    </div>
-                </div>
-            </div>
-            <div class="col-md-2">
-                Level 3: .col-sm-3
-            </div>
-            <div class="col-md-2">
-                Level 4: .col-sm-3
-            </div>
-            <div class="col-md-2">
-                Level 5: .col-sm-3
-            </div>
-            <div class="col-md-2">
-                Level 6: .col-sm-3
-            </div>
-        </div>
-        {{-- <div class="row ">
-            <div class="col-md-6">
-                <label for="fotoDelJugador" class="form-label">Nombre de Equipo:</label>
-            </div>
-            <div class="col-md-6">
-                <label for="fotoDelJugador" class="form-label">Altura:</label>
-            </div>
-            <div class="col-md-6">
-                <label for="fotoDelJugador" class="form-label">Peso:</label>
-            </div>
-            <div class="col-md-6">
-                <label for="fotoDelJugador" class="form-label">Edad:</label>
-            </div>
-            <div class="col-md-6">
-                <label for="fotoDelJugador" class="form-label">Nacionalidad:</label>
-            </div>
-            <div class="col-md-6">
-                <label for="fotoDelJugador" class="form-label">Fecha de Nacimiento:</label>
-            </div>
-            <div class="col-md-6">
-                <label for="fotoDelJugador" class="form-label">Camiseta:</label>
-            </div>
-        </div> --}}
-    </section>
+    </div>
+
 </div>
 @endsection
