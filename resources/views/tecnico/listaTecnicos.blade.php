7717f89f57dea3831251ab0340aeecffe4f4e33a
Maxjhon5 <jhonsor55@gmail.com>
2022-10-31 22:16:10 -0400

    mesaje de actualizacion correcta

--- resources/views/tecnico/listaTecnicos.blade.php
+++ resources/views/tecnico/listaTecnicos.blade.php
@@ -43,6 +43,18 @@
                 </ul>
             </nav>
         </header>
+
+        @php
+            $vadido = "¡Valido!";
+            $noVadido = "¡No valido!";
+        @endphp
+        @if (Session::has('mensaje'))
+            <div class="alert alert-success alert-dismissible col-8 d-flex justify-content-center mt-3 mx-auto pt-2 pb-2">
+                <button type="button" class="btn-close" data-bs-dismiss="alert"></button>
+                <h4><strong>{{$vadido}}</strong>{{" "}}{{Session::get('mensaje')}}</h4>
+            </div>
+        @endif
+
         <div class="relative  items-top justify-center min-h-screen  sm:items-center py-4 sm:pt-0 ">
                 <div class="bg-image w-100" >
                     <div class="mask d-flex align-items-center w-100">
