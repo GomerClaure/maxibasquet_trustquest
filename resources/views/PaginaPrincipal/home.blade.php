69dc979585117dcf2add736fd653fb4ce7dc5d03
Luisa Mora <luisamora895@gmail.com>
2022-11-01 23:17:48 -0400

    casi terminado

--- resources/views/PaginaPrincipal/home.blade.php
+++ resources/views/PaginaPrincipal/home.blade.php
@@ -20,12 +20,74 @@
 </div>
 </div>
 
-
+<h2 class="titulo"> Convocatoria </h2>
 <div class="convocatoria">
-  <h2 class="titulo"> Convovatoria </h2>
-  <img class="imagenConv" src="">
+  <img class="imgconv" src="{{asset('storage').'/'.'img_Home'.'/'.'convo.jpg'}}">
+  <img class="imgconv" src="{{asset('storage').'/'.'img_Home'.'/'.'convocatoriax.png'}}">
 </div>
 
-@endsection
+<h1 class="titulo">Noticias</h1>
+<div class="noticias"> 
+   <div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">MAR DEL PLATA ARGENTINA 2023</h5><br><br>
+    <p class="card-text">
+16 Campeona Mundial de Maxibásquetbol FIMBA
+Mar del Plata, Buenos Aires, Argentina 2023.
+
+En los próximos días anunciaremos la fecha exacta, y se realizará la apertura de inscripción.</p>
+    
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">SPALDING & FIMBA</h5><br><br>
+    <p class="card-text">
+Dos potencias del básquetbol mundial se han juntado en una alianza histórica, para elevar al maxibásquetbol a niveles nunca antes vistos.
+Spalding es el balón oficial y sponsor de FIMBA, en todas las competencias oficiales.</p>
   
+  </div>
+</div>
+
+<div class="card" style="width: 18rem;">
+  <div class="card-body">
+    <h5 class="card-title">MALAGA 2020 REPROGRAMADO</h5><br>
+    <br>
+    <p class="card-text">
+Fecha: desde el 26 de junio al 5 de julio, 2020. Fecha pospuesta por la situación del Covid-19.
+Nueva fecha reprogramada: desde el 25 de junio al 4 de julio, 2021. Fecha pospuesta por la situación del Covid-19.
+Fecha definitiva: desde el 24 de junio al 3 de julio de 2022.</p>
+  </div>
+</div>
+<img class="notas" src="{{asset('storage').'/'.'img_Home'.'/'.'2_2_12.jpg'}}">
+</div>
+<br><br><section class="">
+  <!-- Footer -->
+  <footer class="text-center text-white" style="background-color: #444b52;">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+      <!-- Section: CTA -->
+      <section class="">
+        <p class="d-flex justify-content-center align-items-center">
+          <span class="me-3">Campeonato Maxi Basquet</span>
+          <button type="button" class="btn btn-outline-light btn-rounded">
+            Sign up!
+          </button>
+        </p>
+      </section>
+      <!-- Section: CTA -->
+    </div>
+    <!-- Grid container -->
+
+    <!-- Copyright -->
+    <div class="text-center p-3" style="background-color: rgba(0, 0, 0, 0.2);">
+      © 2020 Copyright:
+      <a class="text-white" href="https://mdbootstrap.com/">TrustQuest.com</a>
+    </div>
+    <!-- Copyright -->
+  </footer>
+  <!-- Footer -->
+</section>
+@endsection
 </html>
