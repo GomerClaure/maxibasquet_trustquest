da2709001649ee2cad46da114027bbab3407c69a
Gomer Claure <gomerclaureb@gmail.com>
2022-11-11 14:36:50 -0400

    Cambiando la contraseña apassword

--- database/factories/UserFactory.php
+++ database/factories/UserFactory.php
@@ -18,7 +18,7 @@ class UserFactory extends Factory
             'name' => $this->faker->words(2, true),
             'email' => $this->faker->unique()->safeEmail(),
             'email_verified_at' => now(),
-            'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'password' => '$2y$10$9Rs9YLHXnxXQCY0NQnqbjOZhUAK7BFOO.2ml6ytO1kij2PTA8O6ua', // password
             'remember_token' => Str::random(10),
         ];
     }
