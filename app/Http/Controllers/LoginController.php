b19419a630cc8e5bd1a0a8f42153ef060bc87765
Gomer Claure <gomerclaureb@gmail.com>
2022-11-18 13:06:52 -0400

    Agregando slider

--- app/Http/Controllers/LoginController.php
+++ app/Http/Controllers/LoginController.php
@@ -31,18 +31,24 @@ class LoginController extends Controller
         $usuario = User::where('name',$formulario[$this->nombreUsuario]) -> first();
         
         if($usuario){
+            echo 'Entra al login';
             $nomUsuario = $usuario->name;
             $contra = $usuario->password;
+            echo $contra;
             if (Hash::check($formulario[$this->contrasenia], $contra)) {
                 $credentials = $this->getLoginRequest($formulario);
                 // print_r($credentials);
                 $userAuth = Auth::getProvider()->retrieveByCredentials($credentials);
                 Auth::login($userAuth);
                 echo "Las contraseñas coinciden";
-                return redirect('/home');
-            }  
+                // return redirect('/home');
+            }else{
+                echo "contraseña incorrecta wey";
+            }
+        }else {
+            echo "No encuentra al usuarios";
         }
-        return back()->withInput()->with('errorLogin','El nombre de usuario o contraseña es incorrecto.');
+        // return back()->withInput()->with('errorLogin','El nombre de usuario o contraseña es incorrecto.');
     }
 
     private function getLoginRequest($formulario){
