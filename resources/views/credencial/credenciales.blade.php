2ddb4d1d886339d77135cccb7efcff143092ad93
Luisa Mora <luisamora895@gmail.com>
2022-12-05 10:22:49 -0400

    Footer

--- resources/views/credencial/credenciales.blade.php
+++ resources/views/credencial/credenciales.blade.php
@@ -147,6 +147,7 @@
             </div>
         </div>
     </body>
+    <br><br><br><br><br><br><br><br>
     @endsection
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
 </html>
