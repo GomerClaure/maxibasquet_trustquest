510ee7fa5f41d3ae0385c3139b13b6abcb72728c
jhosmanB <102555068+jhosmanB@users.noreply.github.com>
2022-12-06 00:50:25 -0400

    arreglandor bug de collapse

--- resources/views/credencial/credenciales.blade.php
+++ resources/views/credencial/credenciales.blade.php
@@ -5,7 +5,6 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>Credeenciales Equipo</title>
         <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css">
-        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
         <link rel="stylesheet" href="{{asset('css/StyleCredenciales.css')}}">
     </head>
     @extends('nav')
@@ -149,8 +148,7 @@
                 </div>
             </div>
         </div>
-    </body>
-    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
+    </body>  
     @endsection
     
 </html>
