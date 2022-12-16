f15f9ff0813a92857f02b3888b17adf940d696f9
Maxjhon5 <76125954+Maxjhon5@users.noreply.github.com>
2022-12-16 00:09:32 -0400

    Update mostrar.blade.php

--- resources/views/fixtur/mostrar.blade.php
+++ resources/views/fixtur/mostrar.blade.php
@@ -92,6 +92,7 @@
                             </div>
                         
         </div>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/js/bootstrap.bundle.min.js"></script>
         @endsection
 
      </body>
